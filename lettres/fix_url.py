e20794f40ebbfc63b9d2e5b8e833558d94e29d88
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2023-02-10 13:43:22 +0100

    Update fix_url.py

--- lettres/fix_url.py
+++ lettres/fix_url.py
@@ -1,11 +1,19 @@
 import sys, os
 
-path = os.path.join(sys.argv[1])
-text = open( path ).read()
-words = text.strip().replace("\n"," ").split()
-urls = filter( lambda word: word.startswith("http"), words)
-
-for url in set(urls):
-    text = text.replace( url, "<"+url+">" )
-
-open( path, 'w' ).write(text)
+if len(sys.argv) < 2:
+	print("""
+	 Pour utiliser ce script python ajouter le nom du fichier md
+	 example:
+	    python fix_url.py 31-janvier-2023.md
+	""")
+else:
+	
+	path = os.path.join(sys.argv[1])
+	text = open( path ).read()
+	words = text.strip().replace("\n"," ").split()
+	urls = filter( lambda word: word.startswith("http"), words)
+	
+	for url in set(urls):
+	    text = text.replace( url, "<"+url+">" )
+	
+	open( path, 'w' ).write(text)
